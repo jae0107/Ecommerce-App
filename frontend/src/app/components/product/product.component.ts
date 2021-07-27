162d1f660f4210332992c25d0de8e3cf2a4795f7
jae0107 <ghjgjh0107@gmail.com>
2021-07-28 05:28:00 +0930

    product ts

--- frontend/src/app/components/product/product.component.ts
+++ frontend/src/app/components/product/product.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
+import { map } from "rxjs/operators";
+
+declare let $: any;
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent implements OnInit {
-  id: Number;
+export class ProductComponent implements OnInit, AfterViewInit {
+  id: number;
   product;
   thumbimages: any[] = [];
 
@@ -20,7 +23,61 @@ export class ProductComponent implements OnInit {
               private cartService: CartService) { }
 
   ngOnInit(): void {
-    
+    this.route.paramMap.pipe(
+      map((param: ParamMap) => {
+        // @ts-ignore
+        return param.params.id;
+      })
+    ).subscribe(prodId => {
+      this.id = prodId;
+      
+      this.productService.getSingleProduct(this.id).subscribe(prod => {
+        this.product = prod;
+
+        if (prod.images !== null) {
+          this.thumbimages = prod.images.split(';');
+        }
+      });
+    });
+  }
+
+  ngAfterViewInit(): void {
+    // Product Main img Slick
+    $('#product-main-img').slick({
+      infinite: true,
+      speed: 300,
+      dots: false,
+      arrows: true,
+      fade: true,
+      asNavFor: '#product-imgs',
+    });
+
+    // Product imgs Slick
+    $('#product-imgs').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: true,
+      centerMode: true,
+      focusOnSelect: true,
+      centerPadding: 0,
+      vertical: true,
+      asNavFor: '#product-main-img',
+      responsive: [{
+        breakpoint: 991,
+        settings: {
+          vertical: false,
+          arrows: false,
+          dots: true,
+        }
+      },
+      ]
+    });
+
+    // Product img zoom
+    let zoomMainProduct = document.getElementById('product-main-img');
+    if (zoomMainProduct) {
+      $('#product-main-img .product-preview').zoom();
+    }
   }
 
   addToCart(id: number) {

b1f860a115e0d85d91d0a7a88148b82380cb17b2
jae0107 <ghjgjh0107@gmail.com>
2021-07-28 04:35:10 +0930

    init product component

--- frontend/src/app/components/product/product.component.ts
+++ frontend/src/app/components/product/product.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CartService } from 'src/app/services/cart.service';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product',
@@ -6,10 +9,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
+  id: Number;
+  product;
+  thumbimages: any[] = [];
 
-  constructor() { }
+  @ViewChild('quantity') quantityInput;
+
+  constructor(private route: ActivatedRoute,
+              private productService: ProductService,
+              private cartService: CartService) { }
 
   ngOnInit(): void {
+    
   }
 
+  addToCart(id: number) {
+    this.cartService.AddProductToCart(id, this.quantityInput.nativeElement.value);
+  }
+
+  Increase() {
+    let value = parseInt(this.quantityInput.nativeElement.value);
+    if (this.product.quantity >= 1){
+      value++;
+
+      if (value > this.product.quantity) {
+        value = this.product.quantity;
+      }
+    } else {
+      return;
+    }
+
+    this.quantityInput.nativeElement.value = value.toString();
+  }
+
+  Decrease() {
+    let value = parseInt(this.quantityInput.nativeElement.value);
+    if (this.product.quantity > 0){
+      value--;
+
+      if (value <= 0) {
+        value = 0;
+      }
+    } else {
+      return;
+    }
+    this.quantityInput.nativeElement.value = value.toString();
+  }
 }
